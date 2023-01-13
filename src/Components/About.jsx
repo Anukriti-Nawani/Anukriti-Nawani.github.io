1cd57d9142bd5a593c0cfcf5623de9dd95974ea5
Anukriti-Nawani <anunwn@gmail.com>
2023-01-13 15:17:31 +0530

    changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,7 @@
 import DownloadIcon from '@mui/icons-material/Download';
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import myImage from '../Resources/Images/profilePic.jpeg'
+import myImage from '../Resources/Images/profile.jpeg'
 import backgroundImage from '../Resources/Images/backgroundHome.jpeg';
 
 import Typical from "react-typical";
